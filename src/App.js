44ee83266af1962eccb59be5ba31186b46178622
unknown <2020pgcaca51@nitjsr.ac.in>
2023-09-25 02:11:47 +0530

    ok tested

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from 'react'
-import {
-  BrowserRouter as Router, 
-  Routes,
-  Route
-} from 'react-router-dom'
-import Layout from './layout/Layout'
-import Home from './components/pages/Home'
-// import AboutUs from './components/pages/AboutUs'
-// import ContactUs from './components/pages/ContactUs'
+
+import ContactUs from './components/misc/ContactUs'
+import NewProducts from './components/misc/NewProducts'
+import Header from './components/misc/Header'
+import MenuBar from './components/navigations/MenuBar'
+import Footer from './components/navigations/Footer'
 
 import './App.css';
 
 function App() {
   return (
-    <div className="body-wrap">
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path={'/'} component={Home}></Route>
-            {/* <Route path={'/AboutUs'} component={AboutUs}></Route>
-            <Route path={'/ContactUs'} component={ContactUs}></Route> */}
-          </Routes>
-        </Layout>
-      </Router>
+    <div className="App">
+      <MenuBar />
+      <Header/>
+      <NewProducts/>
+      <ContactUs/>
+      <Footer />
     </div>
   );
 }
